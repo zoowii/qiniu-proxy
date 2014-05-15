5399b6c3815aca51a46dfc18d8fd00f6da3ccbef
zoowii <1992zhouwei@gmail.com>
2014-05-15 23:32:27 +0800

    fix some TODO

--- src/qiniu_proxy/routes/api.clj
+++ src/qiniu_proxy/routes/api.clj
@@ -14,10 +14,6 @@
             [clojure.tools.logging :as logging]
             [qiniu-proxy.utils :as utils]))
 
-(defn hi-api
-  []
-  "hello")
-
 (defn- do-upload
   ([params]
    (do-upload params (utils/rand-str 40)))
@@ -35,9 +31,9 @@
   [{qiniu-key    :qiniu_key qiniu-secret :qiniu_secret qiniu-bucket :qiniu_bucket
     content-type :content_type upload-type :upload_type {file-size :size file :tempfile} :file filepath :filepath
     return-type  :return_type webhook-url :web_hook_url :as params} file-key]
-  (let [_ (do-upload params file-key)]
-    (client/get (str webhook-url "?success=true&file-size=-1&store_key=" file-key))
-    (logging/info "hi, agent done")))                       ;; TODO: change file-size
+  (let [{file-size :file-size :as _} (do-upload params file-key)]
+    (client/get (str webhook-url "?success=true&file-size=" file-size "&store_key=" file-key))
+    (logging/info "hi, agent done")))
 
 (defn upload-handler
   "接收上传文件请求并处理"
@@ -47,18 +43,14 @@
   (do
     (println params upload-type file return-type)
     (case return-type
-      "direct" (json/write-str {:success true :file-size file-size :store_key (do-upload params)})
+      "direct" (json/write-str {:success true :file-size file-size :store_key (:store-key (do-upload params))})
       "web_hook" (let [file-key (utils/rand-str 40)]
                    (send (agent params) #(do-upload-and-callbak % file-key))
                    (json/write-str {:success true :store_key file-key}))
       "error")
-    ;; 暂时只直接上传
-    ;(let [file-key (do-upload params)]
-    ;  (json/write-str {:success true :file_size file-size :store_key file-key}))
     ))
 
 (defroutes api-routes
-           (GET "/api/v1/hi" [] (hi-api))
            (mp/wrap-multipart-params
              (POST "/api/v1/upload"
                    {params :params}
--- src/qiniu_proxy/utils.clj
+++ src/qiniu_proxy/utils.clj
@@ -3,7 +3,7 @@
            (com.qiniu.api.auth.digest Mac)
            (com.qiniu.api.rs PutPolicy)
            (com.qiniu.api.io PutExtra IoApi)
-           (java.io InputStream Reader)
+           (java.io InputStream Reader File)
            (java.util Properties))
   (:require [clojure.tools.logging :as logging]))
 
@@ -33,13 +33,15 @@
   ([key secret bucket ^InputStream stream content-type]
    (upload-binary-stream key secret bucket ^InputStream stream content-type (rand-str 40)))
   ([key secret bucket ^InputStream stream content-type file-key]
-   (let [uptoken (get-uptoken key secret bucket)
+   (let [file-size (.available stream)
+         uptoken (get-uptoken key secret bucket)
          extra (PutExtra.)
          _ (set! (.mimeType extra) content-type)
          _ (IoApi/Put uptoken file-key stream extra)
          _ (.close stream)]
      (logging/info (str "upload done " file-key))
-     file-key)))
+     {:store-key file-key
+      :file-size file-size})))
 
 (defn upload-local-file
   ([key secret bucket filepath content-type]
@@ -50,4 +52,5 @@
          ;_ (set! (.mimeType extra) content-type)
          _ (IoApi/putFile uptoken file-key filepath extra)]
      (logging/info (str "upload done " file-key))
-     file-key)))
+     {:store-key file-key
+      :file-size (.length (File. filepath))})))
